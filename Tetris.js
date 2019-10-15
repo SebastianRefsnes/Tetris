3fcb549a2a212d8edb638772a984a034143130c4
Sebastian Refsnes <sebastian.refsnes@gmail.com>
2019-10-15 21:41:35 +0200

    Initial commit

---
+++ Tetris.js
@@ -0,0 +1,77 @@
+window.onload = startGame;
+canvas = document.getElementById("tetris")
+ctx = canvas.getContext("2d");
+frames = 60;
+grid = {x:10,y:20};
+blockSize = {x:canvas.width/grid.x, y: canvas.height/grid.y}
+floor = [canvas.height,canvas.height,canvas.height,canvas.height,canvas.height,canvas.height,canvas.height,canvas.height,canvas.height,canvas.height]
+stored = [];
+gridMemory = [[]];
+lastTetromino = "";
+loopID = "";
+
+function startGame(){
+  tetromino = new Tetromino();
+  for(let i = 0; i < grid.y+4; i++){
+      gridMemory[i] =[0,0,0,0,0,0,0,0,0,0,0];
+  }
+  loopID = setInterval(game,1000/frames);
+}
+update = true;
+function game(){
+
+  //Drawing background
+  ctx.fillStyle = "black"
+  ctx.fillRect(0,0,canvas.width,canvas.height);
+
+  //Drawing the grid
+  //X plane
+  for(let i = 0; i < grid.x; i++){
+    ctx.beginPath();
+    ctx.moveTo(i*(canvas.width/grid.x),0);
+    ctx.strokeStyle = "gray";
+    ctx.lineTo(i*(canvas.width/grid.x),canvas.height);
+    ctx.stroke();
+  }
+
+  //Y plane
+  for(let i = 0; i < grid.y; i++){
+    ctx.beginPath();
+    ctx.moveTo(0,i*(canvas.height/grid.y));
+    ctx.strokeStyle = "gray";
+    ctx.lineTo(canvas.width,i*(canvas.height/grid.y));
+    ctx.stroke();
+  }
+
+
+  //Getting new tetromino if needed
+  if(tetromino.focus === false){
+    stored.push(tetromino);
+    lastTetromino = tetromino;
+    tetromino = new Tetromino();
+  }
+
+  //Checking if game should end
+  if(tetromino.y <= 0 && lastTetromino.y <= 0){
+    clearInterval(loopID);
+    stored = [];
+    alert("Game over!")
+    return;
+  }
+
+  //Drawing old tetromino objects
+  if(stored.length){
+    stored.forEach((tetrominos,i) => {
+      if(tetrominos.y === canvas.height || tetrominos.height <= 0){
+        stored.splice(i);
+      }
+          tetrominos.draw();
+    });
+  }
+
+  tetromino.removeLines();
+  tetromino.update();
+  tetromino.merge();
+  tetromino.draw();
+
+}
---
+++ Tetromino.js
@@ -0,0 +1,366 @@
+class Tetromino{
+
+  constructor(){
+    this.type = "s";
+    this.color = "red";
+    this.descend = 1;
+    this.size = 10;
+    this.focus = true;
+    this.x = 4*blockSize.x;
+    this.y = 0;
+    this.start = new Date();
+    this.gravityTimer = new Date();
+    this.memory = [[]];
+    this.rotation = 0;
+
+    this.randomType();
+
+    for(let i = 0; i < grid.y+4; i++){
+        this.memory[i] =[0,0,0,0,0,0,0,0,0,0,0];
+    }
+
+
+    switch(this.type){
+      case "i":
+        this.shape = [[1,0],
+                      [1,0],
+                      [1,0],
+                      [1,0]];
+        this.color = "cyan";
+        this.height = 4;
+        break;
+      case "o":
+      this.shape = [[0,0],
+                    [1,1],
+                    [1,1],
+                    [0,0]];
+
+      this.color = "yellow";
+      this.height = 2;
+        break;
+      case "t":
+      this.shape = [[0,0],
+                    [0,1],
+                    [1,1],
+                    [0,1]];
+      this.color = "purple";
+      this.height = 3;
+        break;
+      case "j":
+      this.shape = [[0,0],
+                    [0,1],
+                    [0,1],
+                    [1,1]];
+      this.color = "blue";
+      this.height = 3;
+        break;
+      case "l":
+      this.shape = [[0,0],
+                    [1,0],
+                    [1,0],
+                    [1,1]];
+      this.color = "orange";
+      this.height = 3;
+          break;
+      case "s":
+      this.shape = [[0,1],
+                    [1,1],
+                    [1,0],
+                    [0,0]];
+      this.color = "green";
+      this.height = 3;
+          break;
+      case "z":
+      this.shape = [[1,0],
+                    [1,1],
+                    [0,1],
+                    [0,0]];
+      this.color = "red";
+      this.height = 3;
+          break;
+    }
+
+    self = this;
+
+  }
+
+
+  rotate(){
+
+  }
+
+  update(){
+    if(!this.focus) return;
+    let now = new Date();
+    let blockSize = {x:canvas.width/grid.x, y: canvas.height/grid.y}
+
+    //Gravity thingy
+    if(input.space){
+      if(now-this.gravityTimer>10){
+        this.y+=blockSize.y;
+        this.gravityTimer = new Date();
+      }
+    }
+      if(input.down){
+        if(now-this.gravityTimer>50){
+          this.y+=blockSize.y;
+          this.gravityTimer = new Date();
+        }
+    }else if(!input.space && !input.down){
+      if(now-this.gravityTimer>500){
+        this.y+=blockSize.y;
+        this.gravityTimer = new Date();
+      }
+    }
+    //Gravity end
+
+    //Move tetromino along x axis
+    if(input.left && this.x != 0){
+      if(now-this.start>50){
+        this.x-=blockSize.x;
+
+        let reverse = false;
+        gridMemory.forEach((grd,i) => {
+          grd.forEach((innerGrd, j) => {
+            if(this.memory[i][j+1] === 1 && gridMemory[i][j] === 1){
+              reverse = true;
+            }
+          });
+        });
+        if(reverse){
+          this.x += blockSize.x;
+        }
+
+        this.start = new Date();
+      }
+    }
+      if(input.right && this.x != (blockSize.x*grid.x)-blockSize.x*(tetromino.type === "i" ?  1 : 2)){
+        if(now-this.start>100){
+          this.x+=blockSize.x;
+          let reverse = false;
+          gridMemory.forEach((grd,i) => {
+            grd.forEach((innerGrd, j) => {
+              if(this.memory[i][j-1] === 1 && gridMemory[i][j] === 1){
+                reverse = true;
+              }
+            });
+          });
+          if(reverse){
+            this.x -= blockSize.x;
+          }
+          this.start = new Date();
+        }
+      }
+        if(input.up){
+          if(now-this.start>500){
+            this.y-=blockSize.y;
+            this.start = new Date();
+          }
+        }
+
+        this.storePosition();
+  }
+
+  storePosition(){
+  //Store new position values
+
+  //Clear
+  for(let i = 0; i < (grid.y+4); i++){
+      this.memory[i] =[0,0,0,0,0,0,0,0,0,0];
+  }
+  //Looping through shape
+  for(let i = 0; i < this.shape.length; i++){
+  //Left side
+  if(this.shape[i][0]===1){
+    //console.log((this.y/blockSize.y)+i, this.x/blockSize.x);
+    this.memory[(this.y/blockSize.y)+i][this.x/blockSize.x] = 1;
+  }
+  //Right side
+  if(this.shape[i][1]===1){
+    this.memory[(this.y/blockSize.y)+i][(this.x/blockSize.x)+1] = 1;
+  }
+}
+}
+
+  //Merge handles collision detection. Also removes focus if collision on y axis is detected.
+  merge(){
+    if(!this.focus) return;
+    let blockSize = {x:canvas.width/grid.x, y: canvas.height/grid.y};
+
+    //Collision check
+    let leftSide = {start:0,end:0, edited:false};
+    let rightSide = {start:0,end:0, edited:false};
+
+    this.shape.forEach((shape,i) => {
+
+      //Doing blank bottom check
+      if(this.type === "o" || this.type === "s" || this.type === "z"){
+        if (this.y+(blockSize.y*3)>= canvas.height){
+            this.focus = false;
+          }
+      } else if (this.y+(blockSize.y*4)>= canvas.height){
+          this.focus = false;
+        }
+
+        //Catching all other collision cases.
+        if(this.focus){
+        let reverse = false;
+        gridMemory.forEach((grd,i) => {
+          grd.forEach((innerGrd, j) => {
+            let c = i;
+            if(this.memory[i][j] === 1 && gridMemory[i][j] === 1){
+              reverse = true;
+            }
+          });
+        });
+        if(reverse){
+          this.focus = false;
+          this.y -= blockSize.y
+          this.storePosition();
+        }
+      }
+
+    //Finally, removing focus and storing memory to grid
+    if(!this.focus){
+      this.memory.forEach((tetMem, i) => {
+        for(let v = 0; v < 10; v++){
+          if(this.memory[i][v] === 1){
+            gridMemory[i][v] = 1;
+          }
+        }
+      });
+    }
+  });
+}
+
+  swapGridElements(arr,a,b){
+    let tempA = a;
+    arr[a] = arr[b];
+    arr[b] = arr[tempA];
+
+  }
+
+
+  removeLines(){
+    let lines = [];
+    let inLine = [];
+    gridMemory.forEach((grd, i) => {
+      if(JSON.stringify(gridMemory[i])===JSON.stringify([1,1,1,1,1,1,1,1,1,1,0]) && i <= 19){
+        lines.push(i);
+      }
+    });
+
+    if(lines.length !== 0){
+      console.log(lines)
+
+      //After this I have to swap the previous elements to simulate the grid memory "dropping down"
+      for(let i = 0; i < lines.length; i++){
+        gridMemory[lines[i]] =[0,0,0,0,0,0,0,0,0,0,0];
+      }
+      this.swapGridElements(gridMemory,lines[lines.length-1],lines.length);
+
+      //Remove lines from shape and store
+      let toRemove = lines.length;
+      this.shape.forEach((temShape,i) => {
+        if(this.shape[i].includes(1)){
+          this.shape[i] = [0,0];
+          this.storePosition();
+        }
+      });
+
+      //Moves and stores new points
+      this.y += blockSize.y*2;
+      this.storePosition();
+    }
+
+  }
+
+  draw(){
+    ctx.fillStyle = this.color;
+    ctx.strokeStyle = "white";
+
+    //Drawing tetromino
+    let blockSize = {x:canvas.width/grid.x, y: canvas.height/grid.y}
+    for(let i = 0; i < 4; i++){
+
+      if(this.shape[i][0]===1){
+        ctx.fillRect(this.x,this.y+(i*blockSize.y),blockSize.x,blockSize.y);
+        ctx.strokeRect(this.x,this.y+(i*blockSize.y),blockSize.x,blockSize.y);
+
+      }
+       if(this.shape[i][1]===1){
+        ctx.fillRect(this.x+blockSize.x,this.y+(i*blockSize.y),blockSize.x,blockSize.y);
+        ctx.strokeRect(this.x+blockSize.x,this.y+(i*blockSize.y),blockSize.x,blockSize.y);
+
+      }
+
+
+    }
+  }
+
+  randomType(){
+   let result = '';
+   let characters = 'iotjlsz';
+  characters = "o";
+   let charLength = characters.length;
+   for ( let i = 0; i < 1; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charLength));
+   }
+   this.type=result;
+}
+
+
+}
+
+input = { left:false,
+          right:false,
+          up:false,
+          down:false,
+          space:false  };
+
+window.onkeydown = e => {
+  switch(e.keyCode){
+    case 32:
+      input.space = true;
+      break;
+      case 37:
+         input.left = true;
+      break;
+
+      case 38:
+        input.up = true;
+      break;
+
+      case 39:
+        input.right = true;
+      break;
+
+      case 40:
+        input.down = true;
+      break;
+
+  }
+}
+window.onkeyup = e =>{
+  switch(e.keyCode){
+    case 32:
+      input.space = false;
+      break;
+      case 37:
+         input.left = false;
+      break;
+
+      case 38:
+        input.up = false;
+      break;
+
+      case 39:
+        input.right = false;
+      break;
+
+      case 40:
+        input.down = false;
+      break;
+
+  }
+}
